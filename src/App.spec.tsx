2389a2758ea86d5ee730f0960e31553424e8c194
Chris <chrisbcl@hotmail.com>
2021-02-12 19:02:08 +0000

    Remove smoke tests

--- src/App.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import App from './App';
-
-describe('App', () => {
-    it('renders Hi', () => {
-        const { getByTestId } = render(<App />);
-        expect(getByTestId('greeting').textContent).toBe('Hi');
-    });
-});
